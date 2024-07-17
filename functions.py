af165acc776d8dac472a7627febfdd4717d829ce
harish-babu <harish.babu@gmail.com>
2024-07-17 15:46:35 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import ast
 import re
 import pandas as pd
 import json
+from tenacity import retry, wait_random_exponential, stop_after_attempt
 
 # This function initiates create the system and role conversation with Open AI model
 def initialize_conversation():
@@ -85,6 +86,42 @@ def get_chat_model_completions(messages):
         #max_tokens = 300
     )
     return response.choices[0].message.content
+
+
+
+
+# Define a Chat Completions API call
+# Retry up to 6 times with exponential backoff, starting at 1 second and maxing out at 20 seconds delay
+@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+def get_chat_completions(input, json_format = False):
+    MODEL = 'gpt-3.5-turbo'
+
+    system_message_json_output = """<<. Return output in JSON format to the key output.>>"""
+
+    # If the output is required to be in JSON format
+    if json_format == True:
+        # Append the input prompt to include JSON response as specified by OpenAI
+        input[0]['content'] += system_message_json_output
+
+        # JSON return type specified
+        chat_completion_json = openai.chat.completions.create(
+            model = MODEL,
+            messages = input,
+            response_format = { "type": "json_object"},
+            seed = 1234)
+
+        output = json.loads(chat_completion_json.choices[0].message.content)
+
+    # No JSON return type specified
+    else:
+        chat_completion = openai.chat.completions.create(
+            model = MODEL,
+            messages = input,
+            seed = 2345)
+
+        output = chat_completion.choices[0].message.content
+
+    return output
 
 
 # The following fucntion checks the user input for content inputed for moderation check
